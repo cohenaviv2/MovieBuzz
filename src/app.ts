5df2a095878cebe73ec73c74d2cc35150fc8be59
Aviv Cohen <119066085+cohenaviv2@users.noreply.github.com>
2023-12-18 15:40:14 +0200

    Implementing movies service

--- src/app.ts
+++ src/app.ts
@@ -10,5 +10,16 @@ app.use("/movies", movieRouter);
 app.use("/tv",tvShowsRouter);
 
 app.listen(port, () => {
-  console.log(`\n\n* Server is running on http://localhost:${port} *\n\n`);
+  console.log(`\n\nServer is running on http://localhost:${port}\n\nMovies:\n
+http://localhost:5000/movies/search\n
+http://localhost:5000/movies/popular\n
+http://localhost:5000/movies/now_playing\n
+http://localhost:5000/movies/upcoming\n
+http://localhost:5000/movies/by-genre/:genreId\n
+Tv Shows:\n
+http://localhost:5000/tv/search\n
+http://localhost:5000/tv/popular\n
+http://localhost:5000/tv/top_rated\n
+http://localhost:5000/tv/on_the_air\n
+http://localhost:5000/tv/by-genre/:genreId`);
 });
